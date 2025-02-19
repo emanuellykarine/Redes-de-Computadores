22bf59b02c429e78f2fea18774d2269824e5a010
Rebeca Noemi <rebecanoemi494@gmail.com>
2025-02-18 22:33:18 -0300

    Create Cliente.py

---
+++ Project/Cliente.py
@@ -0,0 +1,22 @@
+import socket #Utilizado para criar conexões de rede
+
+class Cliente:
+    def __init__(self) -> None:
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Cria o socket utilizando IPv4 (protocolo de endereço) e TCP (tipo do socket)
+        self.s.connect((socket.gethostname(), 5551)) #Conecta o socket à máquina local na porta 5551
+
+    def solicitar_info(self) -> None:
+        # Envia um comando fixo para o servidor solicitar as informações do sistema
+        self.s.send(b"get_info")
+
+    def fechar_conexao(self) -> None:
+        self.s.close()
+
+def main():
+    cliente = Cliente()
+    cliente.solicitar_info()
+    cliente.fechar_conexao()
+
+if __name__ == "__main__":
+    main()
+

d1c45a5d45f000821c46ad29e0a8419225843773
Emanuelly Karine <89488185+emanuellykarine@users.noreply.github.com>
2025-03-08 12:18:50 -0300

    Create Sistema.py

---
+++ V4/Sistema.py
@@ -0,0 +1,33 @@
+import psutil
+import random
+# import wmi
+class Sistema:
+    @staticmethod
+    def espaco_livre_hd() -> str:
+        resposta = psutil.disk_usage('/').free / (1024**3)  # conversão para GB
+        return f"{resposta:.2f} GB"
+
+    @staticmethod
+    def qtd_processadores() -> str:
+        resposta = psutil.cpu_count(logical=True)
+        return f"{resposta}"
+
+    @staticmethod
+    def espaco_memoria() -> str:
+        resposta = psutil.virtual_memory().available / (1024**3)  # memória disponível em GB
+        return f"{resposta:.2f} GB"
+    
+    @staticmethod
+    def temperatura() -> int:
+        resposta = random.randint(40, 80)
+        return f"{resposta}°C"
+    
+    # @staticmethod
+    # def temperatura() -> int:
+    #     w = wmi.WMI(namespace="root\\wmi")
+    #     temperaturas = w.MSAcpi_ThermalZoneTemperature()
+    #     for temp in temperaturas:
+    #          if temp.Name and "CPU" in temp.Name:
+    #                 # A temperatura é retornada em décimos de Kelvin, então convertemos para Celsius
+    #                 resposta = (temp.CurrentTemperature / 10.0) - 273.15
+    #                 return f"Temperatura do processador: {resposta:.2f}°C"
